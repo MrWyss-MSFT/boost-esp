954d9a1ccf9ddb8183a1a20e7a55904a0c0b4a9f
MrWyss-MSFT <marius.wyss@microsoft.com>
2022-03-29 17:02:10 +0200

    oops loop

--- BootStrap-BoostESP.ps1
+++ BootStrap-BoostESP.ps1
@@ -1,2 +1,2 @@
-$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/MrWyss-MSFT/boost-esp/main/BootStrap-BoostESP.ps1
+$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/MrWyss-MSFT/boost-esp/main/Boost-ESP.ps1
 Invoke-Expression $($ScriptFromGithHub.Content)
